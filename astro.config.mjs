1e610fdf74ccc80f6e89a2184d963c623caebf9d
G-AOBptyltd <gcollocott@agilityopsbusinessptyltd.com>
2026-01-28 00:25:33 +1100

    Update astro.config.mjs // import sitemap from '@astrojs/sitemap';
    // import sitemap from '@astrojs/sitemap';

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://villagefirst.org.au',
-  integrations: [sitemap()],
+  integrations: [],
   output: 'static',
   build: {
     format: 'directory'
